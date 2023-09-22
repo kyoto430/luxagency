f60daa1ac5286043f1013aa1a06ecf906e1a0a3b
Kyoto430 <nateriver345@gmail.com>
2023-09-22 21:22:34 +0300

    feat: add burger & backgrounds

---
+++ assets/js/index.js
@@ -0,0 +1,35 @@
+// Меню бургер
+function burger() {
+  console.log('burger');
+  const burgerBtn = document.querySelector('.menu__burger');
+  const menu = document.querySelector('.header__nav');
+  const menuLinks = document.querySelectorAll('.menu__link');
+  const overlay = document.querySelector('.overlay');
+
+  burgerBtn.addEventListener('click', function () {
+    document.body.classList.toggle('lock');
+    burgerBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+    overlay.classList.toggle('active');
+  });
+
+  menuLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+      document.body.classList.remove('lock');
+      burgerBtn.classList.remove('active');
+      menu.classList.remove('active');
+      overlay.classList.remove('active');
+    });
+  });
+
+  document.addEventListener('click', function (event) {
+    if (!menu.contains(event.target) && !burgerBtn.contains(event.target)) {
+      document.body.classList.remove('lock');
+      burgerBtn.classList.remove('active');
+      menu.classList.remove('active');
+      overlay.classList.remove('active');
+    }
+  });
+}
+
+burger();
